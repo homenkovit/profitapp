2f437bbab1cf1056643d9ebdcf10bb7ffcdfd43e
norzserge <metelskiisa@mail.ru>
2021-10-20 21:09:53 +0300

    Burger menu (#10)
    * component popover base version
    
    * ts-ignore
    
    * add new lines
    
    * small refactor
    
    * small refactor
    
    * add aria
    
    * add aria to menu buttons
    
    * rename component
    
    * rename class name
    
    * add whitespace
    
    * small design changes
    
    * remove keys from li
    
    * fixes after review
    
    * add blank line

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Header } from './components/header/header';
 import { JobList } from './components/job-list/job-list';
 import { SortBar } from './components/sort-bar/sort-bar';
-import { ReactComponent as IconSettings } from './assets/images/settings.svg';
-import { ReactComponent as IconLogout } from './assets/images/logout.svg';
 import styles from './app.module.scss';
 import './base.css';
 import './resources/styles/modules.scss';
 import { user } from './mocks';
 import { GreetingMessage } from './components/greeting-message/greeting-message';
+import { TopBarRightActions } from './components/top-bar-right-actions/top-bar-right-actions';
 
 const App = () => (
   <div className={styles.app}>
@@ -16,12 +15,7 @@ const App = () => (
     <main>
       <div className={styles['top-bar']}>
         <SortBar />
-        <button type='button' aria-label='settings' className={styles['settings-button']}>
-          <IconSettings aria-hidden />
-        </button>
-        <a href='#' aria-label='logout' className={styles.logout}>
-          <IconLogout aria-hidden />
-        </a>
+        <TopBarRightActions />
       </div>
       { /* TODO: add visibility by auth and onLogIn / onRegister methods */ }
       <GreetingMessage
---
+++ src/components/popover/popover.tsx
@@ -0,0 +1,82 @@
+import React, { FC, ReactElement, useRef, ReactNode } from 'react';
+import { Placement } from '@popperjs/core';
+import Tippy from '@tippyjs/react/headless';
+import { Instance } from 'tippy.js';
+import styles from './popover.module.scss';
+
+export interface PopoverProps {
+  visible?: boolean;
+  className?: string;
+  placement?: Placement;
+  role?: string;
+  children?: ReactElement;
+  content?: ReactNode;
+  onMount?: (instance: Instance) => void;
+}
+
+export const Popover: FC<PopoverProps> = (props) => {
+  const popoverRef = useRef<Instance>(null);
+  const popoverContentRef = useRef<HTMLDivElement>(null);
+
+  let trigger: string | undefined;
+  if (props.visible === undefined) {
+    trigger = 'click';
+  }
+
+  const onPopoverRootKeyDown = (event: React.KeyboardEvent<HTMLDivElement>): void => {
+    if (event.key === 'Escape') {
+      if (popoverRef.current) {
+        popoverRef.current.hide();
+      }
+    }
+  };
+
+  const onPopoverMount = (instance: Instance): void => {
+    // @ts-ignore
+    popoverRef.current = instance;
+    if (popoverContentRef.current) {
+      popoverContentRef.current.focus();
+    }
+    if (props.onMount) {
+      props.onMount(instance);
+    }
+  };
+
+  return (
+    <Tippy
+      visible={props.visible}
+      placement={props.placement}
+      interactive
+      trigger={trigger}
+      appendTo={(): HTMLElement => document.body}
+      onMount={onPopoverMount}
+      render={(attrs): ReactElement => {
+        if (props.visible !== false) {
+          return (
+            <div
+              role={props.role || 'dialog'}
+              className={`${styles.popover} ${props.className ?? ''}`}
+              onKeyDown={onPopoverRootKeyDown}
+              {...attrs}
+            >
+              <div
+                ref={popoverContentRef}
+                tabIndex={-1}
+                className={styles.content}
+              >
+                {props.content}
+              </div>
+              <div data-popper-arrow="" className={styles.arrow}></div>
+            </div>
+          )
+        } return <div />;
+      }}
+    >
+      {props.children}
+    </Tippy>
+  )
+};
+
+Popover.defaultProps = {
+  placement: "bottom",
+}
