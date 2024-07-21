e3f6f9b27ab566bb2b98e23713e5d588c8246bd6
justaman <homenkovit@gmail.com>
2024-07-21 19:11:04 +0200

    test

--- src/components/order-item-card/order-item-card.tsx
+++ src/components/order-item-card/order-item-card.tsx
@@ -1,4 +1,4 @@
-import { FC, memo, useLayoutEffect, useRef, useState } from 'react'
+import { FC, memo, useRef } from 'react'
 
 import IconOverdue from 'assets/images/overdue.svg?react'
 
@@ -6,7 +6,7 @@ import { decodeText } from 'global/helpers'
 import type { Order } from 'contexts/order-context'
 
 import { getCardStyles } from './helpers'
-import { ExpandableCardDescription } from './components/expandable-card-description'
+// import { ExpandableCardDescription } from './components/expandable-card-description'
 import { CardLabel } from './components/card-label'
 import { CardActions } from './components/card-actions'
 import styles from './order-item-card.module.scss'
@@ -19,29 +19,12 @@ interface OrderItemCardProperties {
 const OrderItemCard: FC<OrderItemCardProperties> = ({ data, onClickEdit }) => {
   const descriptionReference = useRef<HTMLParagraphElement>(null)
 
-  const [isExpandButtonVisible, setExpandButtonVisible] = useState<boolean>(false)
-
-  useLayoutEffect(() => {
-    const description = descriptionReference.current
-    if (description) {
-      if (description.offsetWidth < description.scrollWidth) {
-        setExpandButtonVisible(true)
-      } else {
-        setExpandButtonVisible(false)
-      }
-    }
-  }, [data.description])
-
   return (
     <div className={getCardStyles(styles, data)}>
-      {isExpandButtonVisible ? (
-        <ExpandableCardDescription description={data.description} isOrderOverdue={Boolean(data.isOverdue)} />
-      ) : (
-        <p className={styles.description} ref={descriptionReference}>
-          {data.isOverdue && <IconOverdue className={styles['overdue-icon']} />}
-          {decodeText(data.description)}
-        </p>
-      )}
+      <p className={styles.description} ref={descriptionReference}>
+        {data.isOverdue && <IconOverdue className={styles['overdue-icon']} />}
+        {decodeText(data.description)}
+      </p>
       <div className={styles['last-row']}>
         <strong className={styles.price}>
           {data.price.toLocaleString('ru-RU', {
